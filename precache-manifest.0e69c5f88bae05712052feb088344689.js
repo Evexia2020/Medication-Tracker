a5718304ae15921371601d99320e77ecb23149fa
Syndicate555 <saziz028@uottawa.ca>
2020-06-21 14:56:03 -0400

    Updates

---
+++ precache-manifest.0e69c5f88bae05712052feb088344689.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab90b26073a6b9d39c4b2dca02f03f5c",
+    "url": "/Medication-Tracker/index.html"
+  },
+  {
+    "revision": "1d10f8dadf80cf394817",
+    "url": "/Medication-Tracker/static/css/main.961c995a.chunk.css"
+  },
+  {
+    "revision": "adee47c34e7866edce16",
+    "url": "/Medication-Tracker/static/js/2.aa4893d2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Medication-Tracker/static/js/2.aa4893d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d10f8dadf80cf394817",
+    "url": "/Medication-Tracker/static/js/main.17812e34.chunk.js"
+  },
+  {
+    "revision": "d376a7bd0bdf1a0f6467",
+    "url": "/Medication-Tracker/static/js/runtime-main.95b7a0e5.js"
+  },
+  {
+    "revision": "b9fb1f4e9c2bb2ae0d260a660e8a43c4",
+    "url": "/Medication-Tracker/static/media/profile.b9fb1f4e.png"
+  }
+]);
